ff1e0aacd4b8e28380ab27b3a41221f587ad51f4
Joey <grey.lila@yahoo.com>
2018-05-15 08:56:22 -0500

    Add files via upload

---
+++ interactWithGift.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class interactWithGift : MonoBehaviour {
+    public Canvas gui;
+
+    void OnMouseDown()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Destroy(gameObject);
+            print("was destroyed");
+            gui.GetComponent<guibehavior>().UpdateGUI();
+        }
+    }
+
+}
